a03545c2af1150a00c1f3702dbff157e723ca5b4
eroome <152181984+eroome@users.noreply.github.com>
2023-11-27 13:05:42 +0000

    Add files via upload

---
+++ init_compo.f
@@ -0,0 +1,66 @@
+!                    *********************
+                     SUBROUTINE INIT_COMPO
+!                    *********************
+!
+     &(NCOUCHES)
+!
+!***********************************************************************
+! SISYPHE   V7P2
+!***********************************************************************
+!| NCOUCHES       |-->| NUMBER OF LAYER FOR EACH POINT
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!
+      USE BIEF
+      USE DECLARATIONS_TELEMAC
+      USE DECLARATIONS_SISYPHE
+      USE DECLARATIONS_SPECIAL
+      IMPLICIT NONE
+!
+!+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+!
+!                                       NPOIN
+      INTEGER, INTENT (INOUT)::NCOUCHES(*)
+!
+!+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+!
+      INTEGER I,J,K,IERR
+      DOUBLE PRECISION EPAI
+!      INTEGER, PARAMETER :: NPOIN = 324863
+!      DOUBLE PRECISION, DIMENSION(NPOIN, NCOUCHES, NSICLA) :: AVAIL
+!      DOUBLE PRECISION, DIMENSION(NPOIN, NCOUCHES) :: ES
+      DOUBLE PRECISION :: FRAC_C(NPOIN)
+      CHARACTER*16 VAR_NAME
+!
+!-----------------------------------------------------------------------
+!
+!  TO BE FILLED BY THE USER
+
+      WRITE(LU,*) 'start reading sediment fractions'
+      WRITE(LU,*) 'from file: ',SIS_FILES(SISGEO)%NAME    
+      DO I=1,NSICLA        
+         WRITE(VAR_NAME,'(a4,I1,a11)')'FRAC',I,'           '
+         WRITE(LU,*)'variable name:',VAR_NAME
+         CALL FIND_VARIABLE('SERAFIN ',SIS_FILES(SISGEO)%LU,
+     &                   VAR_NAME,FRAC_C, NPOIN,
+     &                   IERR,0d0)
+         IF (IERR.eq.0) THEN
+            WRITE(LU,*) 'found fraction FRAC',I
+            WRITE(LU,*) 'range=',minval(FRAC_C),maxval(FRAC_C)
+         ELSE
+            WRITE(LU,*) 'fraction NOT found for FRAC',I
+         ENDIF
+         DO J=1,NPOIN
+            NCOUCHES(J) = 1
+            AVAIL(J,1,I) = FRAC_C(J)
+            DO K=1,NCOUCHES(J)
+               ES(J,K)=(ZF%R(J)-ZR%R(J))/NCOUCHES(J)
+            ENDDO
+         ENDDO
+      ENDDO
+
+      ! USER FUNCTION
+      CALL USER_INIT_COMPO(NCOUCHES)
+
+      RETURN
+      END
+
